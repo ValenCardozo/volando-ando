3d3ded4ae18e84cc8c3f9fe877cf43ad951aee49
Matias Gomez <matiias.gomeez999@gmail.com>
2025-06-25 21:16:27 -0300

    add model flight

--- fly_project/home/models.py
+++ fly_project/home/models.py
@@ -9,3 +9,23 @@ class Airplane(models.Model):
 
     def __str__(self):
         return f"{self.model} (Capacity: {self.capacity})"
+
+class Flight(models.Model):
+    STATUSES = [
+        ("scheduled", "Scheduled"),
+        ("in_flight", "In flight"),
+        ("completed", "Completed"),
+        ("canceled", "Canceled"),
+    ]
+    airplane = models.ForeignKey(Airplane, on_delete=models.PROTECT, related_name="flights")
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    duration = models.DurationField()
+    status = models.CharField(max_length=20, choices=STATUSES, default="scheduled")
+    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+    users = models.ManyToManyField(User, related_name="managed_flights", blank=True)
+
+    def __str__(self):
+        return f"Flight {self.id} - {self.origin} to {self.destination}"
