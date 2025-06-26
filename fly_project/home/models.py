067814fbb981b87dce4df780fb08a6b9f4086611
Matias Gomez <matiias.gomeez999@gmail.com>
2025-06-25 21:14:43 -0300

    add model Airplane

--- fly_project/home/models.py
+++ fly_project/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Airplane(models.Model):
+    model = models.CharField(max_length=100)
+    capacity = models.PositiveIntegerField()
+    rows = models.PositiveIntegerField()
+    columns = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.model} (Capacity: {self.capacity})"
