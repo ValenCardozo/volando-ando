bc188ef55a98fa38bc238a22b60aed2476829fc8
ValenCardozo <valencardozo7@gmail.com>
2025-06-29 16:42:23 -0300

    feat: move models

--- fly_project/fly_project/settings.py
+++ fly_project/fly_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.request',
             ],
         },
     },
@@ -103,9 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'es'
 
 USE_I18N = True
+USE_L10N = True
 
 LANGUAGES = [
     ('es', 'Español'),
--- fly_project/home/admin.py
+++ fly_project/home/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from app.models import Airplane, Flight, Passenger, Seat, Reservation, Ticket
 
-# Register your models here.
+@admin.register(Airplane)
+class AirplaneAdmin(admin.ModelAdmin):
+    list_display = ('model', 'capacity', 'rows', 'columns')
+    search_fields = ('model',)
+    list_filter = ('capacity',)
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ('id', 'origin', 'destination', 'departure_time', 'arrival_time', 'status', 'base_price')
+    list_filter = ('status', 'origin', 'destination', 'departure_time')
+    search_fields = ('origin', 'destination')
+
+@admin.register(Passenger)
+class PassengerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'document', 'document_type', 'email', 'phone')
+    search_fields = ('name', 'document', 'email')
+    list_filter = ('document_type',)
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ('number', 'row', 'column', 'type', 'status', 'get_airplane_model')
+    list_filter = ('status', 'type', 'airplane__model')
+    search_fields = ('number',)
+
+    def get_airplane_model(self, obj):
+        return obj.airplane.model
+    get_airplane_model.short_description = 'Airplane Model'
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('reservation_code', 'get_flight_info', 'get_passenger_name', 'status', 'reservation_date', 'price')
+    list_filter = ('status', 'reservation_date', 'flight__origin', 'flight__destination')
+    search_fields = ('reservation_code', 'passenger__name', 'flight__origin', 'flight__destination')
+
+    def get_flight_info(self, obj):
+        return f"{obj.flight.origin} → {obj.flight.destination}"
+    get_flight_info.short_description = 'Flight'
+
+    def get_passenger_name(self, obj):
+        return obj.passenger.name
+    get_passenger_name.short_description = 'Passenger'
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('barcode', 'get_passenger', 'get_flight', 'issue_date', 'status')
+    list_filter = ('status', 'issue_date')
+    search_fields = ('barcode', 'reservation__reservation_code')
+
+    def get_passenger(self, obj):
+        return obj.reservation.passenger.name
+    get_passenger.short_description = 'Passenger'
+
+    def get_flight(self, obj):
+        flight = obj.reservation.flight
+        return f"{flight.origin} → {flight.destination}"
+    get_flight.short_description = 'Flight'
