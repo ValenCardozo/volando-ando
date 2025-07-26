841b1b2803a8369ac2ff981c4d6b308bcf685932
Matias Gomez <matiias.gomeez999@gmail.com>
2025-07-26 17:08:28 -0300

    code enhacements

--- fly_project/home/urls.py
+++ fly_project/home/urls.py
@@ -4,12 +4,24 @@ from home.views import (
     HomeView,
     LoginView,
     LogoutView,
-    RegisterView
+    RegisterView,
+    OffersView,
+    BuyOfferView,
+    MyFlightsView,
+    DeleteReservationView,
+    ConfirmReservationView,
+    FinalizeReservationView,
 )
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('offers/', OffersView.as_view(), name='offers'),
+    path('buy-offer/<int:flight_id>/', BuyOfferView.as_view(), name='buy_offer'),
+    path('my-flights/', MyFlightsView.as_view(), name='my_flights'),
+    path('delete-reservation/<int:reservation_id>/', DeleteReservationView.as_view(), name='delete_reservation'),
+    path('confirm-reservation/<int:flight_id>/', ConfirmReservationView.as_view(), name='confirm_reservation_prompt'),
+    path('finalize-reservation/<int:flight_id>/', FinalizeReservationView.as_view(), name='finalize_reservation'),
     path('', HomeView.as_view(), name='index'),
 ]
